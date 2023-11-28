e1d5bd74473de99805feb0e0870b0fe97dbb1d15
Pham Thi Minh Hau <ptmhau@avaya.com>
2023-11-28 13:52:10 +0700

    filter exercise

--- src/filter.js
+++ src/filter.js
@@ -4,17 +4,24 @@ const persons = data.persons;
 const personsHasAgeGreaterThanParams = (age) => {
   // Code here: return persons with age greater than parameter
   // Example: return persons.filter(person => person.age > age);
+  return persons.filter(person => person.age > age)
 }
 const personsHasAddressEqualParams = (address) => {
   // Code here: return persons with address equal to parameter
+  return persons.filter(person => person.address === address)
 }
 
 const getNames = (age, address) => {
   // Code here: return name of persons with address equal to parameter
+  const filterData = persons.filter(person => person.address === address && person.age === age)
+  const nameOfFiltered = filterData.map(person => person.name)
+  return nameOfFiltered
 }
 
 const personsWhoIsMember = () => {
   // Code here: return persons who is member
+  const filterMember = persons.filter(person => person.member)
+  return filterMember
 }
 
 module.exports = {
